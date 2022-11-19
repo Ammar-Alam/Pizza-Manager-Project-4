16a4dcf0b157c7b793ea197e501fa6eba0869e00
Ammar-Alam <ammaralam37@gmail.com>
2022-11-19 18:15:02 -0500

    Implemented order class

--- src/main/java/pizzaclasses/Order.java
+++ src/main/java/pizzaclasses/Order.java
@@ -1,6 +1,7 @@
 package pizzaclasses;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 /**
  * @author Ammar A
@@ -15,6 +16,15 @@ public class Order implements Customizable{
      * List of pizzas to order
      */
     private ArrayList<Pizza> orderItems;
+
+    /**
+     * Constructor for order
+     * @param orderNum Order number
+     */
+    public Order(int orderNum) {
+        this.orderNum = orderNum;
+    }
+
     /**
      * Adds an object
      *
@@ -38,10 +48,52 @@ public class Order implements Customizable{
      */
     @Override
     public boolean remove(Object obj) {
-        if(obj instanceof Pizza && this.orderItems.contains((((Pizza) obj))){
+        if(obj instanceof Pizza && this.orderItems.contains((((Pizza) obj)))){
             this.orderItems.remove(obj);
             return true;
         }
         return false;
+    }
+
+    /**
+     * Getter for order number
+     * @return Return order number
+     */
+    public int getOrderNum() {
+        return orderNum;
+    }
+
+    /**
+     * Getter for list of pizzas in order
+     * @return ArrayList of pizzas
+     */
+    public ArrayList<Pizza> getOrderItems() {
+        return orderItems;
+    }
+
+    /**
+     * New equals method
+     * @param o Object to compare
+     * @return Return true if equal, false otherwise
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return orderNum == order.orderNum;
+    }
+
+    /**
+     * Creates string representation of order
+     * @return Returns string form of order
+     */
+    @Override
+    public String toString() {
+        String output = "Order number " + orderNum;
+        for(Pizza pizza : orderItems){
+            output += "\t" + pizza.toString();
+        }
+        return output;
     }
 }
