d92009dad0d4856aad5c69508b5256f73e635309
Ammar-Alam <ammaralam37@gmail.com>
2022-11-20 00:15:29 -0500

    Added variables for all GUI elements used

--- src/main/java/com/gui/pizzamanagerproject4/NYStylePizzaOrderingController.java
+++ src/main/java/com/gui/pizzamanagerproject4/NYStylePizzaOrderingController.java
@@ -1,9 +1,41 @@
 package com.gui.pizzamanagerproject4;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+
 /**
  * Controller For NY Style Pizza Ordering View
  * @author Ammar A
  * @author Nikhil G
  */
 public class NYStylePizzaOrderingController {
+
+    @FXML
+    private ListView<String> availableToppings;
+
+    @FXML
+    private TextField crustTypeDisplay;
+
+    @FXML
+    private RadioButton largeCrust;
+
+    @FXML
+    private RadioButton medCrust;
+
+    @FXML
+    private ComboBox<String> pizzaFlavorSelection;
+
+    @FXML
+    private ListView<?> selectedToppings;
+
+    @FXML
+    private ToggleGroup size;
+
+    @FXML
+    private RadioButton smallCrust;
+
 }
