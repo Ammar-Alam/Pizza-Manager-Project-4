754f6534c4f7e9e51108811228261a9c96d9ef37
Ammar-Alam <ammaralam37@gmail.com>
2022-11-20 00:14:13 -0500

    Added variables for all GUI elements used

--- src/main/java/com/gui/pizzamanagerproject4/MainViewController.java
+++ src/main/java/com/gui/pizzamanagerproject4/MainViewController.java
@@ -1,7 +1,8 @@
 package com.gui.pizzamanagerproject4;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 
 /**
  * Controller for Main View
@@ -9,11 +10,61 @@ import javafx.scene.control.Label;
  * @author Nikhil G
  */
 public class MainViewController {
-    @FXML
-    private Label welcomeText;
 
+    /**
+     * Button to order Chicago style pizza
+     */
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button orderChicagoStyle;
+
+    /**
+     * Button to order New York style pizza
+     */
+    @FXML
+    private Button orderNYStyle;
+
+    /**
+     * Button to see your current order
+     */
+    @FXML
+    private Button showOrderButton;
+
+    /**
+     * Button to see store orders
+     */
+    @FXML
+    private Button storeOrdersButton;
+
+    /**
+     * Starts process to order a Chicago style pizza
+     * @param event Triggering event
+     */
+    @FXML
+    void orderChicagoPizza(ActionEvent event) {
     }
+
+    /**
+     * Starts process to order a New York style pizza
+     * @param event Triggering event
+     */
+    @FXML
+    void orderNYPizza(ActionEvent event) {
+    }
+
+    /**
+     * Starts process to view and edit your current order
+     * @param event Triggering event
+     */
+    @FXML
+    void viewOrder(ActionEvent event) {
+    }
+
+    /**
+     * Starts process to view the current store orders
+     * @param event Triggering event
+     */
+    @FXML
+    void viewStoreOrders(ActionEvent event) {
+    }
+
 }
