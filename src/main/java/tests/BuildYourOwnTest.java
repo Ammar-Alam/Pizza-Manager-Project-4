76f62aa0b580a554fc85fa69dc132a40ea9a9225
Ammar-Alam <ammaralam37@gmail.com>
2022-11-21 03:25:36 -0500

    Initial commit of JUnit test file

---
+++ src/main/java/tests/BuildYourOwnTest.java
@@ -0,0 +1,10 @@
+package tests;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BuildYourOwnTest {
+
+    @org.junit.jupiter.api.Test
+    void testPrice() {
+        // TODO add test
+    }
+}
