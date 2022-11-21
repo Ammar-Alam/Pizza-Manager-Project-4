255eef0da03514b1f4db8763849058621a8968f9
Ammar-Alam <ammaralam37@gmail.com>
2022-11-21 18:44:04 -0500

    Added module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+/**
+ * Module containing all Java files
+ * @author Ammar A
+ * @author Nikhil G
+ */
+module com.gui.pizzamanagerproject4 {
+        requires javafx.controls;
+        requires javafx.fxml;
+        requires org.junit.jupiter.api;
+
+
+        opens com.gui.pizzamanagerproject4 to javafx.fxml;
+        exports com.gui.pizzamanagerproject4;
+}
