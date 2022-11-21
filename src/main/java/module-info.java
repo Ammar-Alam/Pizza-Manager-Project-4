1a955d7103f3dbdda279e598ef5e6c1e6827165e
Ammar-Alam <ammaralam37@gmail.com>
2022-11-21 03:25:16 -0500

    Change made for JUnit testing requirements

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,7 @@
 module com.gui.pizzamanagerproject4 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.junit.jupiter.api;
 
 
     opens com.gui.pizzamanagerproject4 to javafx.fxml;
