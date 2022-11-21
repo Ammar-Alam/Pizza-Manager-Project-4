eaa4ab75347ed1c7ebe45ef813cf5c4b955d3f15
Ammar-Alam <ammaralam37@gmail.com>
2022-11-20 22:27:10 -0500

    Added a static variable where all store orders are stored

--- src/main/java/com/gui/pizzamanagerproject4/PizzaManagerMain.java
+++ src/main/java/com/gui/pizzamanagerproject4/PizzaManagerMain.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import pizzaclasses.StoreOrders;
 
 import java.io.IOException;
 
@@ -13,11 +14,16 @@ import java.io.IOException;
  * @author Nikhil G
  */
 public class PizzaManagerMain extends Application {
+    /**
+     * Store orders object
+     */
+    public static StoreOrders storeOrders = new StoreOrders();
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(PizzaManagerMain.class.getResource("MainView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("RU Pizza");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
